35d3ff9e1eb4c4339fe606d38d36a2fdd5eb14b8
Anna McIntosh <anlautens@gmail.com>
2021-01-19 15:14:53 -0500

    Updating database url

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 
 //set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.vhs6l.mongodb.net/local-library?retryWrites=true&w=majority`;
+const mongoDB = `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.vhs6l.mongodb.net/local-library?retryWrites=true&w=majority`;
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error'));
